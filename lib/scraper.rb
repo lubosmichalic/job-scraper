863dd7052c84d9669917e6c39b17a0026c460e88
Lubos Michalic <lnmichalic@gmail.com>
2017-10-03 15:43:32 +0100

    Add httparty to be able to fetch document page data from the site

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,17 @@
+require 'HTTParty'
+
 class Scraper
+  JOBONLINE_URL = 'http://jobonline.thecareersgroup.co.uk/careersgroup/student/Vacancies.aspx?&st='
+
   def initialize
     @data = ''
   end
 
   def check_data
     @data
+  end
+
+  def retrieve_page
+    @data = HTTParty.get(JOBONLINE_URL)
   end
 end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -6,4 +6,9 @@ describe Scraper do
   it 'is instantiated with an empty string' do
     expect(scraper.check_data).to eq ''
   end
+
+  it 'is able to retrieve document data from JobOnline' do
+    scraper.retrieve_page
+    expect(scraper.retrieve_page).to include('Vacancies / Search results')
+  end
 end
